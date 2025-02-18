d37ac3e183b62e521694d0cf7422a474913778cc
Tanvi Laad <132342600+tanvilaad@users.noreply.github.com>
2025-02-18 20:54:03 +0530

    added multistage docker

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello Dosto'
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
