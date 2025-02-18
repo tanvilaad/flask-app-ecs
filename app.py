d84a0a851af29025529b4c50cf1ae55088268a20
Tanvi Laad <132342600+tanvilaad@users.noreply.github.com>
2025-02-18 21:24:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Dosto'
+    return 'The Flask App ECS is a simple web app built using the Flask framework, containerized with Docker, and deployed on Amazon ECS (Elastic Container Service). It is designed to be scalable and easy to manage in the cloud environment. The app runs on port 80 and uses ECS for container orchestration, making it suitable for cloud-based applications that need to be scaled efficiently.'
 
 @app.route('/health')
 def health():
